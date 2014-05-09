1c9722458c5e9d3b60429bbd7eb3e55db01cde8d
csalva <cristinaasalva@gmail.com>
2014-05-09 16:42:13 +0200

    Update transacciones.py

--- transacciones.py
+++ transacciones.py
@@ -1,3 +1,20 @@
 __author__ = 'cristina'
 
+
 class Transaccion():
+    def __init__(self, identificacion,dni,iban,saldo):
+        self.nombreTitular=identificacion
+        self.nif=dni
+        self.numeroCuenta=iban
+        self.saldoCuenta=saldo
+
+    def buscarCliente(self,identificacion):
+        with open('cuentas.txt', mode='r', encoding='utf-8')as archivo:
+            for i in archivo:
+                nombreTitular,nif,numeroCuenta,saldoCuenta = i.split('-',4);
+
+
+
+    #def Ingresar(self):
+
+    #def Retirar(self):
