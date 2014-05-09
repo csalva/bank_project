02fad53f8f07aa398dfa29cd5bdf70546318f1e3
csalva <cristinaasalva@gmail.com>
2014-05-09 16:43:18 +0200

    Update particular.py

--- particular.py
+++ particular.py
@@ -3,16 +3,12 @@ __author__ = 'cristina'
 from random import randrange
 
 class Particular():
-    def __init__(self, nombreParticular,apellidosParticular,nif):
+    def __init__(self, nombreParticular,nif):
         self.nombreParticular = nombreParticular
-        self.apellidosParticular = apellidosParticular
         self.nif = nif
 
     def getNombreParticular(self):
         return self.nombreParticular
-
-    def getApellidosParticular(self):
-        return self.apellidosParticular
 
     def getNif(self):
         return self.nif
@@ -23,7 +19,7 @@ class Particular():
         iban += " "+str(randrange(10,99))
         iban += " "+str(randrange(1000000000,9999999999))
 
-        registro = (self.nombreParticular+"-"+self.apellidosParticular+"-"+self.nif+"-"+iban+"-"+0,00)
+        registro = (self.nombreParticular+"-"+self.nif+"-"+iban+"-"+"0,00"+"\n")
         with open('cuentas.txt', mode='a', encoding='utf-8')as archivo:
             archivo.write(registro)
             print(registro)
