dd473193aad1f904eb10c8c69cc56dd14a6cb817
csalva <cristinaasalva@gmail.com>
2014-05-09 17:09:14 +0200

    Update transacciones.py

--- transacciones.py
+++ transacciones.py
@@ -2,19 +2,22 @@ __author__ = 'cristina'
 
 
 class Transaccion():
-    def __init__(self, identificacion,dni,iban,saldo):
-        self.nombreTitular=identificacion
-        self.nif=dni
-        self.numeroCuenta=iban
-        self.saldoCuenta=saldo
+    def __init__(self, titular,nif,iban,saldo):
+        self.nombreTitular=titular
+        self.nif=nif
+        self.iban=iban
+        self.saldo=saldo
 
     def buscarCliente(self,identificacion):
         with open('cuentas.txt', mode='r', encoding='utf-8')as archivo:
             for i in archivo:
                 nombreTitular,nif,numeroCuenta,saldoCuenta = i.split('-',4);
-
-
+                if nombreTitular == nombreTitular and nif == nif:
+                    print(nombreTitular,nif,numeroCuenta,saldoCuenta)
+                else:
+                    print("Error de identificacion")
 
     #def Ingresar(self):
 
     #def Retirar(self):
+
